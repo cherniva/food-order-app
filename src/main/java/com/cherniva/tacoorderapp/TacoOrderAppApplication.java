b5304c497d7e23224a61624ed2258ccd84da473a
Ivan Chernikov <chernikoviivan@gmail.com>
2024-02-03 19:53:39 +0100

    Committing application and configuration files

---
+++ src/main/java/com/cherniva/tacoorderapp/TacoOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.cherniva.tacoorderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoOrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoOrderAppApplication.class, args);
+    }
+
+}
